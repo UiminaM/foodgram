077c099ba8217dc41f0ca24316cb854d88edb814
M_Uimina <m2a0s0h@gmail.ru>
2025-10-06 19:47:24 +0300

    k8s

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = "#3e6at!d5k8_^(@ddrgdn*1lq#vc90i)(td8_9+ulz6=3y7eh@"
 
 DEBUG = True
 
-ALLOWED_HOSTS = ["backend", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',') if os.getenv('ALLOWED_HOSTS') else ['*']
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -93,8 +93,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
+STATIC_ROOT = BASE_DIR / "static_django"
 STATIC_URL = "/static/"
-STATIC_ROOT = BASE_DIR / "static"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_ROOT = BASE_DIR / "media"
