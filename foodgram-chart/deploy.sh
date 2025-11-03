3f784023f74469f600be4204cb067bd3a9c7bac9
M_Uimina <m2a0s0h@gmail.ru>
2025-11-03 18:50:06 +0300

    fix

--- foodgram-chart/deploy.sh
+++ foodgram-chart/deploy.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 set -e
 set -a
-
 source .env
-
 set +a
-
-POSTGRES_USER=$(vals get 'ref+vault://secret/foodgram/backend#POSTGRES_USER')
-POSTGRES_DB=$(vals get 'ref+vault://secret/foodgram/backend#POSTGRES_DB')
-POSTGRES_PASSWORD=$(vals get 'ref+vault://secret/foodgram/backend#POSTGRES_PASSWORD')
 
 helm upgrade --install foodgram-helm ./foodgram-helm \
   -n foodgram \
-  -f values.yaml \
-  --set postgresql.auth.username="$POSTGRES_USER" \
-  --set postgresql.auth.database="$POSTGRES_DB" \
-  --set postgresql.auth.postgresPassword="$POSTGRES_PASSWORD"
+  -f values.yaml 
